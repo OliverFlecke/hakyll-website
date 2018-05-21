af27a991278a6b7877af7e81b3f0a45b5c709f8a
Oliver Fleckenstein <Oliverfl@live.dk>
2018-05-21 22:24:07 +0200

    Updated publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,19 @@
 git checkout develop
 
+# Clean and build the site
+stack build
 stack exec site clean
 stack exec site build
 
 git checkout master
+
+# Copy everything to the root
 cp -a _site/* .
+
+# Commit and publish
 git add --all
 git commit -m 'Publish'
 git push
+
+# Checkout devolopment branch afterwards
+git checkout develop
